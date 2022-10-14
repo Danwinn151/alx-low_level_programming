7e5d69a989ab34a8598d111ce66be97c15324077
Dev_Dan10 <89104561+Danwinn151@users.noreply.github.com>
2022-10-14 06:50:52 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char test_str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(test_str);
+	return (0);
+}
+Footer
+© 2022 GitHub, 
